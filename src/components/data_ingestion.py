1e8e4c273ecff46f710b01dfda8384b683d6af5d
Darpan Lekharu <darpanlekharu@gmail.com>
2023-03-25 22:01:04 +0530

    model train

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,8 +22,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -88,5 +88,5 @@ if __name__ == "__main__":
         train_data, test_data
     )
 
-    # modeltrainer = ModelTrainer()
+    modeltrainer = ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
